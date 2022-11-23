b9c98cdcabde57f790c27d8ca9321e4d2a7b49f7
3FDevelop <3f.develop@gmail.com>
2022-11-23 12:55:25 +0200

    Styles and Fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps):JSX.Element{
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>TOP APP</title>
+				<link rel="icon" href="/favicon.ico"/>
+				<link rel="preconnect" href="https://fonts.googleapis.com"/>
+				<link rel="preconnect" href="https://fonts.gstatic.com"/>
+				<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 export default function Home():JSX.Element{
-	const a = 1;
 	return (
-		<div>
-			{a}
-		</div>
+		<>
+			Some Text
+		</>
 	);
 }
