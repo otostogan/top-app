abd7e78bb74ae3c97120b032d4486ef0964ff2df
3FDevelop <3f.develop@gmail.com>
2022-11-24 10:26:01 +0200

    P component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
-import { Button, Htag } from "../components";
+import { Button, Htag, P} from "../components";
 
 export default function Home():JSX.Element{
 	return (
 		<>
 			<Htag tag="h1">
-				Some text
+				Title
 			</Htag>
-			<Button appearance="ghost" arrow="down">
+			<Button appearance="ghost" arrow="right">
 				Some text
 			</Button>
+			<P size="l">
+				Some text	
+			</P>
+			<P size="s">
+				Some text	
+			</P>
+			<P>
+				Some text	
+			</P>
 		</>
 	);
 }
