de208e8c20543b7d697b17a1f14a5d1efacacb22
3FDevelop <3f.develop@gmail.com>
2022-11-25 12:51:46 +0200

    getStaticPaths

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,6 @@ import { MenuItem } from '../interfaces/menu.interface';
 function Home({ menu }: HomeProps): JSX.Element {
 	const [rating, setRating] = useState<number>(4);
 
-	// console.log(menu);
-
 	return (
 		<>
 			<Htag tag='h1'>Заголовок</Htag>
@@ -23,17 +21,30 @@ function Home({ menu }: HomeProps): JSX.Element {
 			<Tag size='s' color='green'>Green</Tag>
 			<Tag color='primary'>Green</Tag>
 			<Rating rating={rating} isEditable setRating={setRating} />
+			<ul>
+				{menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+			</ul>
 		</>
 	);
 }
 
 export default withLayout(Home);
 
-export const getStaticProps: GetStaticProps<HomeProps> = async (context) => {
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
 	const firstCategory = 0;
-	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find", {
-		firstCategory
-	});
+
+	const menuReq = await fetch(process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find", {
+        method: 'POST',
+        headers: {
+        'content-type': 'application/json;charset=UTF-8',
+        },
+        body: JSON.stringify({
+            firstCategory: 0
+        }),
+    });
+
+    const menu:MenuItem[] = await menuReq.json();
+
 	return {
 		props: {
 			menu,
